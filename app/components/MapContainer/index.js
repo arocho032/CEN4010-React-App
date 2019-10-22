2af77a9cd23b5002454ccde82fabf1fbc20de38b
kianmrf <kianmrf@gmail.com>
2019-10-21 21:50:59 -0400

    .env variables ready to use

--- app/components/MapContainer/index.js
+++ app/components/MapContainer/index.js
@@ -3,9 +3,7 @@ import { Map, Marker, GoogleApiWrapper } from 'google-maps-react';
 
 const API_KEY = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
 
-const dotenv = require('dotenv');
-
-console.log(dotenv);
+// console.log(API_KEY)
 
 export class MapContainer extends React.Component {
   render() {
