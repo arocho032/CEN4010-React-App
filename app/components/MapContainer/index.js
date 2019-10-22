8dbdca538c79541ec1a7f562b87cbb8c41f16ca8
kianmrf <kianmrf@gmail.com>
2019-10-22 14:53:57 -0400

    MapComponent loads markers from this.state.stores

--- app/components/MapContainer/index.js
+++ app/components/MapContainer/index.js
@@ -6,15 +6,52 @@ const API_KEY = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
 // console.log(API_KEY)
 
 export class MapContainer extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      stores: [
+        { lat: 25.9087, lng: -80.1403 },
+        { latitude: 25.7574, longitude: -80.3733 },
+      ],
+    };
+  }
+
+  displayMarkers = () =>
+    this.state.stores.map((store, index) => (
+      <Marker
+        key={Marker.id}
+        id={index}
+        position={{
+          lat: store.latitude,
+          lng: store.longitude,
+        }}
+        onClick={() => console.log('You clicked me!')}
+      />
+    ));
+
   render() {
     return (
-      // this.props.google
-      <Map google={this.props} zoom={14}>
-        <Marker onClick={this.onMarkerClick} name="Current location" />
+      /* this.props.google
+       <Map google={this.props.google} zoom={14}>
+         <Marker onClick={this.onMarkerClick} name="Current location" />
+       </Map> */
+      <Map
+        google={this.props}
+        zoom={8}
+        style={mapStyles}
+        initialCenter={{ lat: 25.7617, lng: -80.1918 }}
+      >
+        {this.displayMarkers()}
       </Map>
     );
   }
 }
+
+const mapStyles = {
+  width: '100%',
+  height: '100%',
+};
 
 export default GoogleApiWrapper({
   apiKey: API_KEY,
